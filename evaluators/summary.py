e2702195fd2ac01ef089b43f2353393a7a59c473
VINAY PRITHYANI <vinayprithyani@VINAYs-MacBook-Pro.local>
2025-05-23 12:36:16 -0400

    get summary from llm comparison method

--- evaluators/summary.py
+++ evaluators/summary.py
@@ -15,3 +15,20 @@ def plot_accuracy_summary(results_csv):
     plt.title("LLM Math Evaluation Accuracy on AQuA-RAT")
     plt.tight_layout()
     plt.show()
+
+def plot_accuracy_summary_llm(results_csv):
+    df = pd.read_csv(results_csv)
+    df['llm_judgement'] = df['llm_judgement'].map({'True': True, 'False': False})
+    summary = df.groupby(["model_name", "version"])["llm_judgement"].mean().reset_index()
+    summary["accuracy (%)"] = summary["llm_judgement"] * 100
+
+    plt.figure(figsize=(10, 6))
+    plt.barh(
+        summary["model_name"] + " " + summary["version"],
+        summary["accuracy (%)"]
+    )
+    plt.xlabel("Accuracy (%)")
+    plt.title("LLM Math Evaluation Accuracy on AQuA-RAT")
+    plt.tight_layout()
+    plt.show()
+
