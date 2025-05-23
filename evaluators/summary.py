baa05ff5f2f345d4459c332fa9cd49a1a0516524
VINAY PRITHYANI <vinayprithyani@VINAYs-MacBook-Pro.local>
2025-05-23 13:10:32 -0400

    make dataset function arg

--- evaluators/summary.py
+++ evaluators/summary.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_accuracy_summary(results_csv):
+def plot_accuracy_summary(results_csv, dataset):
     df = pd.read_csv(results_csv)
     summary = df.groupby(["model_name", "version"])["is_correct"].mean().reset_index()
     summary["accuracy (%)"] = summary["is_correct"] * 100
@@ -12,11 +12,11 @@ def plot_accuracy_summary(results_csv):
         summary["accuracy (%)"]
     )
     plt.xlabel("Accuracy (%)")
-    plt.title("LLM Math Evaluation Accuracy on AQuA-RAT")
+    plt.title(f"LLM Math Evaluation Accuracy on {dataset}")
     plt.tight_layout()
     plt.show()
 
-def plot_accuracy_summary_llm(results_csv):
+def plot_accuracy_summary_llm(results_csv, dataset):
     df = pd.read_csv(results_csv)
     df['llm_judgement'] = df['llm_judgement'].map({'True': True, 'False': False})
     summary = df.groupby(["model_name", "version"])["llm_judgement"].mean().reset_index()
@@ -28,7 +28,7 @@ def plot_accuracy_summary_llm(results_csv):
         summary["accuracy (%)"]
     )
     plt.xlabel("Accuracy (%)")
-    plt.title("LLM Math Evaluation Accuracy on AQuA-RAT")
+    plt.title(f"LLM Math Evaluation Accuracy on {dataset}")
     plt.tight_layout()
     plt.show()
 
